3ad2002faaf25ff540f6a59b3231c8562a437e0c
dyone <dyonepaulotrindadedasilva@gmail.com>
2026-09-02 07:53:54 -0300

    adicionei o menu junto com a linha para limpa tela

--- main.c
+++ main.c
@@ -3,39 +3,41 @@
 #include "circular.h"
 #include "produto.h"
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
     
-    int opcao = 100;
+    int opcao;
      
     while( opcao != 7 ) {
-
+    
+        system("clear");
         printf("===== ESTOQUE DA LOJA =====\n"
             "1. Gerenciar Perecíveis (Simples)\n"
             "2. Gerenciar Não Perecíveis (Dupla)\n"
             "3. Gerenciar Promoções (Circular)\n"
             "0. Sair\n");
-            
         printf("\nEscolha uma opção: ");
+        scanf("%d", &opcao);
 
-        
         switch(opcao) {
             case 1:
               
                 break;
             case 2:
-               
+            
                 break;
             case 3:
-               
+
+                break;
             case 0:
 
-                printf("Saindo do programa...\n");
+                system("clear");
+                printf("PROGRAMA ENCERRADO...\n");
                 return 0;
             default:
                 printf("Opção inválida. Tente novamente.\n");
         }
-
-
     }
 }
